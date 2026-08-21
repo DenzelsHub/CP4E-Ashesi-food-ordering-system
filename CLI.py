d885e6c355fdb09a6aab3eb27da07459ed9ccc2c
DenzelsHub <denzelnabruce@gmail.com>
2026-08-21 23:46:50 +0000

    Update imports and fix nutrition lookup

--- CLI.py
+++ CLI.py
@@ -1,5 +1,5 @@
 from testing 5 import Restaurant, MainDish, Beverage, Snack
-from nutrition import get_nutrition_for_dish
+from nutrition_lookup import get_nutrition_for_dish
 from order_model import Order
 
 
